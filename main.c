073c575953107f3ca1fea92831c099885d7e4090
Freez <hudakamel66@gmail.com>
2025-08-24 21:24:56 +0300

    Create main.c for main.dol

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <wiiuse/wiiuse.h>
+
+int main(int argc, char **argv) {
+    // This is the main function where your Wii Homebrew application's code will go.
+    // It's a good practice to start by initializing your video and input devices.
+
+    printf("Hello, Wii World!\n");
+
+    // Your main program loop would typically go here.
+    // For example:
+    // while (1) {
+    //     // Update your application state
+    //     // Render graphics
+    //     // Handle user input
+    // }
+
+    return 0;
+}
